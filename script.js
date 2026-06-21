8c7142e3ef4843bdd077568a1959e6aec6f9c780
renan <renanfsouza135@gmail.com>
2026-06-21 10:49:41 -0300

    upin

--- script.js
+++ script.js
@@ -1,23 +1,26 @@
 /* import { screen } from "./navigation"; */
-
 let sizeList = document.querySelector('.size-list').querySelectorAll('.size-card')
-
 let btnNextSize = document.querySelector('.btn-full')
 
 function fieldSize(nameElement, message) {
+     const radios = document.querySelectorAll(nameElement);
+     let valorTotal 
     // Seleciona todos os radios de tamanho
-    const radios = document.querySelectorAll(nameElement);
     
     // Verifica se algum está marcado
     let selected = false
     Array.from(radios).forEach(element => {
-          if(element.checked) {selected = true}
+          if(element.checked) {
+               selected = true
+               valorTotal = element.dataset.price || element.dataset.priceTopping
+          }
     });
 
     if (!selected) {
          alert(message);
          return
      } else {
+          alert(valorTotal)
           return true
      }
 }
@@ -27,7 +30,18 @@ function openModal() {
      modal.showModal()
 }
 
-document.querySelector('#ice-card').addEventListener('click', openModal)
 
-export const verify = {fieldSize}
+function op(nameElement) {
+     let radios = document.querySelectorAll(nameElement)
+
+     Array.from(radios).forEach(element => {
+          if(element.checked) {selected = true}
+     radios.forEach(element => {
+          alert(element.dataset.price)
+     });
+})}
+
+
+/* document.querySelector('#ice-card').addEventListener('click', op('input[name="size"]'))
+ */export const verify = {fieldSize}
 
